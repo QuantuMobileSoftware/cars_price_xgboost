a75bb5f6ae38bb3f41b3bbb9b68f3431d45f5bb0
exotikh3 <dotavsegor@gmail.com>
2017-02-05 15:22:37 +0200

    complete model

--- prediction_script.py
+++ prediction_script.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
@@ -43,23 +42,27 @@ dataset = pd.get_dummies(dataset)
 # split our dataset to 1. data and target 2. trainee and test
 #
 
-dataset = dataset[:][:5000]
+dataset = dataset[:][:10000]
 
 target = dataset[4].copy()
 data = dataset.drop([4], 1).copy()
 
 train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=0.2, random_state=42)
-print sum(train_target)/len(train_target)
-# #
-# # run xgboost
-# #
-# gbm = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05).fit(train_data, train_target)
+print sum(train_target) / len(train_target)
+
 #
-# preds = gbm.predict(test_data)
+# run xgboost
 #
-# sum = 0.0
-# for i in range(0, len(preds)):
-#     sum+=abs(preds[i]-test_target.iloc[i])
-# sum/=len(preds)
-#
-# print sum
+
+for x in range(1, 5):
+    for gam in range(0.0, 1.1, 0.25):
+        gbm = xgb.XGBClassifier(max_depth=x, n_estimators=300, learning_rate=0.05, gamma=gam).fit(train_data, train_target)
+
+        preds = gbm.predict(test_data)
+
+        sumu = 0.0
+        for i in range(0, len(preds)):
+            sumu += abs(preds[i] - test_target.iloc[i]) / test_target.iloc[i]
+        sumu /= len(preds)
+
+        print "for depth %d on 10 000 examples accurasy is %d \n" % (x, sumu)
